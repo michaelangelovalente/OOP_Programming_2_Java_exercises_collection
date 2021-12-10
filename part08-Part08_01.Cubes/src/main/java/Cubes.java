b238ab6d170b67e22c8b2d43ac1b2f9cc4dca862
Michaelangelovalente <miky10.mv@gmail.com>
2021-12-10 12:58:20 +0100

    part08_01-cubes

---
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if( scanner.hasNextInt() ){
+            //input is integer
+            int n = Integer.valueOf(scanner.nextLine());
+            System.out.println("");
+        }else{
+
+        }
+    }
+}
