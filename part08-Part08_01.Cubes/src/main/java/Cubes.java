30f1fb63947faba732686f66a1fee80a41a6e148
Michaelangelovalente <miky10.mv@gmail.com>
2022-02-23 10:33:51 +0100

    started second part of course

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -1,16 +1,20 @@
 
 import java.util.Scanner;
 
+
 public class Cubes {
-
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        if( scanner.hasNextInt() ){
-            //input is integer
-            int n = Integer.valueOf(scanner.nextLine());
-            System.out.println("");
-        }else{
+        Scanner scan = new Scanner( System.in );
+        String userIn;
 
+        while( true ){
+            userIn = scan.nextLine();
+            if( ( userIn.equals( "end" )) ){
+                break;
+            }
+            System.out.println( (int) Math.pow( (double) Integer.valueOf( userIn ), 3.0 ));
         }
+        
     }
 }
--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,23 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Integer  num = 0, den = 0;
+        
+        while( true ){
+            Integer tmp = Integer.valueOf( scanner.nextLine() ) ;
+            if( tmp == 0){
+                break;
+            }else if( tmp > 0){
+                num += tmp;
+                den++;
+            }            
+        }
+
+        if( num <= 0 ){
+            System.out.println("Cannot calculate the average");
+        }else{
+            System.out.println( ((double) num) / den );
+        }
 
     }
 }
