4da198cac3e496cbc89856b48b0dd381367ad99f
Michaelangelovalente <miky10.mv@gmail.com>
2022-02-24 15:16:10 +0100

    readme

---
+++ part08-Part08_03.LiquidContainers/src/main/java/Container.java
@@ -0,0 +1,35 @@
+public class Container{
+    private Integer liters;
+    public Container(){
+        this.liters = 0;
+    }
+
+
+    public void add( int val ){
+        if( val > 0 ){
+            this.liters += val;
+            if( this.liters >= 100 ){
+                this.liters = 100;
+            } 
+        }
+        
+    }
+
+    public int remove( int val ){
+        this.liters -= val;
+        if( this.liters < 0 ){
+            int tmp = this.liters;
+            this.liters = 0;
+            return val + tmp;
+        }
+        return val;
+    }
+
+    public void moveFrom( Container src, int val ){
+        this.add( src.remove( val ) );
+    }
+
+    public Integer contains( ){
+        return this.liters;
+    }
+}
--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,28 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Container cont1 = new Container();
+        Container cont2 = new Container();
 
         while (true) {
             System.out.print("> ");
 
             String input = scan.nextLine();
+
             if (input.equals("quit")) {
                 break;
             }
 
+            String[] parts = input.split(" ");
+            
+            if( parts[0].equals( "add" ) ){
+                cont1.add( Integer.valueOf( parts[1] ) );
+            }else if( parts[0].equals( "move" ) ){
+                cont1.moveFrom( cont2, Integer.valueOf( parts[1] ) );
+            }else if( parts[0].equals( "remove" ) ){
+                cont1.remove( Integer.valueOf( parts[1] ) );
+            }
+            /*Need to finish!*/
         }
     }
 
