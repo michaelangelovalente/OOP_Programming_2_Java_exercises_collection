f0b5e132011228965deddf2dba71d44e51b72e83
Michaelangelo Valente <miky10.mv@gmail.com>
2022-02-28 16:57:45 +0100

    working on toDoList

--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -5,14 +5,32 @@ public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Container cont1, cont2;
+        cont1 = new Container();
+        cont2 = new Container();
 
         while (true) {
-            System.out.print("> ");
+            /*System.out.print("> ");*/
+            System.out.println("First: " + cont1.toString());
+            System.out.println("Second: " + cont2.toString() +"\n");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            }
+
+            String parts[] = input.split(" ");
+            int val = Integer.valueOf( parts[1] );
+            if( parts[0].equals( "add" )){
+                cont1.add( val ) ;
+            }else if( parts[0].equals( "remove" ) ){
+                cont2.remove( val );
+            }else if( parts[0].equals( "move" ) ){
+                if( cont1.contains() < val ){
+                    val = cont1.contains();
+                }
+                cont1.remove( val );
+                cont2.add( val );
             }
 
         }
