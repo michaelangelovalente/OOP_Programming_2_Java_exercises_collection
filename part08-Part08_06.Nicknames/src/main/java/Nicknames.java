f10ec22d07642f02008ec703efd1acd9dd3509b8
Michaelangelo Valente <miky10.mv@gmail.com>
2022-03-02 13:50:25 +0100

    part08_06 Nicknames

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,14 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap< String, String > nickN = new HashMap<>();
+
+        String extract[] = {"matthew's nickname is matt", "michael's nickname is mix", "arthur's nickname is artie"};
+        for( String sentence : extract ){
+            String namesAndNicks[] = sentence.split( "'s nickname is " );
+            nickN.put( namesAndNicks[0] , namesAndNicks[1] );
+        }
+        
     }
 
 }
